9219fc75d6d47bd594443be5831314165a2eccb4
CiSigep <Cisigep@gmail.com>
2017-10-05 10:30:24 -0400

    Added model and request parameter into model tests

--- SpringBoot/src/main/controller/TestController.java
+++ SpringBoot/src/main/controller/TestController.java
@@ -1,18 +1,24 @@
 package main.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import main.model.TestBean;
+import main.model.TestModel;
 
 @Controller
 public class TestController {
 	
 	@Inject
 	private TestBean test;
+	@Inject
+	private TestModel testm;
 	
 	@RequestMapping("/TEST")
 	@ResponseBody
@@ -30,5 +36,21 @@ public class TestController {
 	public String testPage(){
 		return "testFile";
 	}
+	
+	@RequestMapping("/TESTMODEL")
+	public String testModel(Model m){
+		m.addAttribute("testm", testm);
+		
+		return "testModel";
+	}
+	
+	@RequestMapping("/TESTREQUEST")
+	public String testRequest(@RequestParam(value = "reqdata") String data, Model m){
+		m.addAttribute("REQ", data);
+		
+		return "testRequest";
+	}
+	
+	
 
 }
