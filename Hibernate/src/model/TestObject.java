a1de4b5ca273ef61326ecea956c15f2bccec101c
CiSigep <Cisigep@gmail.com>
2017-10-24 12:09:05 -0400

    Add annotation conversion

--- Hibernate/src/model/TestObject.java
+++ Hibernate/src/model/TestObject.java
@@ -1,8 +1,18 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TESTTABLE")
 public class TestObject {
 	
+	@Id
+	@Column(name="id")
 	private int id;
+	@Column(name="test")
 	private String test;
 	
 	public int getId() {
