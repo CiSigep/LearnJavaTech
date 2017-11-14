a81c4fe316b08ac2cdef2af4e8f597c613a2513e
CiSigep <Cisigep@gmail.com>
2017-11-14 11:44:34 -0500

    Add Mockito

--- JUnit/src/jUnit/TestObject.java
+++ JUnit/src/jUnit/TestObject.java
@@ -1,6 +1,9 @@
 package jUnit;
 
 public class TestObject {
+	
+	private DummyService ds;
+	
 	public String testPhrase(){
 		return "Test Phrase";
 	}
@@ -8,6 +11,23 @@ public class TestObject {
 	public int testMath (){
 		return 2 + 2;
 	}
+
+	public void setDs(DummyService ds) {
+		this.ds = ds;
+	}
 	
+	public int dummyServiceMath(int a, int b) throws Exception{
+		if(ds != null)
+			return ds.add(a, b);
+		
+		throw new Exception("Dummy Service not Initialized");
+	}
+	
+	public String dummyServiceRepeat(String s) throws Exception{
+		if(ds != null)
+			return ds.repeat(s);
+		
+		throw new Exception("Dummy Service not Initialized");
+	}
 
 }
--- JUnit/src/jUnit/TestObjectTest.java
+++ JUnit/src/jUnit/TestObjectTest.java
@@ -3,7 +3,12 @@ package jUnit;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnit44Runner;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TestObjectTest {
 
 	@Test
@@ -18,6 +23,37 @@ public class TestObjectTest {
 		TestObject t = new TestObject();
 		
 		assertEquals(4, t.testMath());
+	}
+	
+	@Test
+	public void testDummyServiceMath(){
+		TestObject t = new TestObject();
+		DummyService ds = Mockito.mock(DummyService.class);
+		t.setDs(ds);
+		
+		Mockito.when(ds.add(2, 3)).thenReturn(5);
+		
+		try {
+			assertEquals(5, t.dummyServiceMath(2, 3));
+		} catch (Exception e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testDummyServiceRepeat(){
+		TestObject t = new TestObject();
+		DummyService ds = Mockito.mock(DummyService.class);
+		t.setDs(ds);
+		
+		Mockito.when(ds.repeat("HI")).thenReturn("HI");
+		
+		try{
+			assertEquals("HI", t.dummyServiceRepeat("HI"));
+		} catch (Exception e){
+			fail();
+		}
+		
 	}
 
 }
