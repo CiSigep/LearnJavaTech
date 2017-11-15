6fc39fce28fba2ded5d165d960851eaea1d57c6a
CiSigep <Cisigep@gmail.com>
2017-11-15 11:10:38 -0500

    Add log4j2 logging

--- Struts2/src/interceptors/CustomInterceptor.java
+++ Struts2/src/interceptors/CustomInterceptor.java
@@ -1,26 +1,31 @@
 package interceptors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.Interceptor;
 
 public class CustomInterceptor implements Interceptor {
+	
+	private static Logger log = LogManager.getLogger(CustomInterceptor.class.getName());
 
 	@Override
 	public void destroy() {
-		System.out.println("CustomInterceptor Destroyed");
+		log.info("CustomInterceptor Destroyed");
 	}
 
 	@Override
 	public void init() {
-		System.out.println("CustomInterceptor Initialized");
+		log.info("CustomInterceptor Initialized");
 	}
 
 	@Override
 	public String intercept(ActionInvocation act) throws Exception {
-		System.out.println("Interception started, invoking action");
+		log.info("Interception started, invoking action");
 		
 		String result = act.invoke();
 		
-		System.out.println("Action completed, completing intercept");
+		log.info("Action completed, completing intercept");
 		
 		return result;
 	}
