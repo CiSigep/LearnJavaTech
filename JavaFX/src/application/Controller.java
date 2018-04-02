cb4e83b20aab12264694b1b6afb96b8102f5df0d
CiSigep <Cisigep@gmail.com>
2018-04-02 11:43:49 -0400

    More Controls & Event Handlers

--- JavaFX/src/application/Controller.java
+++ JavaFX/src/application/Controller.java
@@ -1,5 +1,35 @@
 package application;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
+public class Controller {
+	
+	@FXML
+	private TextField inputField;
+	@FXML
+	private Button bOne;
+	@FXML
+	private Button bTwo;
+
+	@FXML
+	public void onButtonClicked(ActionEvent ae) {
+		System.out.println(inputField.getText());
+		if(bOne.equals(ae.getSource()))
+			System.out.println("Me");
+		else if(bTwo.equals(ae.getSource()))
+			System.out.println("Me Too");
+	}
+	
+	@FXML
+	public void onKeyReleased(KeyEvent ke) {
+		String input = inputField.getText();
+		boolean disableButton = input.isEmpty() || input.trim().isEmpty();
+		bOne.setDisable(disableButton);
+		bTwo.setDisable(disableButton);
+		
+	}
 }
