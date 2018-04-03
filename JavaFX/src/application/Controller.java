be58b40694778ddcf7991bb6d049891d2639cfa1
CiSigep <Cisigep@gmail.com>
2018-04-03 13:58:19 -0400

    Threading for JavaFX

--- JavaFX/src/application/Controller.java
+++ JavaFX/src/application/Controller.java
@@ -1,8 +1,11 @@
 package application;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 
@@ -14,6 +17,10 @@ public class Controller {
 	private Button bOne;
 	@FXML
 	private Button bTwo;
+	@FXML
+	private CheckBox cBox;
+	@FXML
+	private Label lbl;
 
 	@FXML
 	public void onButtonClicked(ActionEvent ae) {
@@ -22,6 +29,36 @@ public class Controller {
 			System.out.println("Me");
 		else if(bTwo.equals(ae.getSource()))
 			System.out.println("Me Too");
+		
+		Runnable runner = new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(5000);
+					
+					Platform.runLater(new Runnable() {
+						
+						@Override
+						public void run() {
+							lbl.setText("Thread Complete");
+							
+						}
+					});
+				} catch(InterruptedException ie) {
+					ie.printStackTrace();
+				}
+				
+			}
+		};
+		
+		new Thread(runner).start();
+		
+		if(cBox.isSelected()) {
+			inputField.clear();
+			bOne.setDisable(true);
+			bTwo.setDisable(true);
+		}
 	}
 	
 	@FXML
@@ -31,5 +68,10 @@ public class Controller {
 		bOne.setDisable(disableButton);
 		bTwo.setDisable(disableButton);
 		
+	}
+	
+	@FXML
+	public void handleChange() {
+		System.out.println("The checkbox is selected: " + cBox.isSelected());
 	}
 }
